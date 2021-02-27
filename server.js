87f6973ec41eb5b864d98aa9d0d34aaa3e78c598
Tom Bates <thomas1602@hotmail.co.uk>
2021-02-27 21:38:26 +0000

    fixed cannot GET

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 console.log("SocketServer running");
 
+// start with express
 var express = require('express'); // these variables are function calls
 var app = express();
 var server = app.listen(3000);
 
-app.use(express.static('public')); // hosts app
+app.use(express.static('public')); 
 
+// setup socket
 var socket = require('socket.io');
 var io = socket(server);
-
 io.sockets.on('connection', newConnection);
-
 function newConnection(socket)
 {
     console.log('new connection: ' + socket.id);
 }
+
+//set up routing
