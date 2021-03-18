803b9c412148c71d3cdcb6f3540bd092f31c3762
Tom Bates <thomas1602@hotmail.co.uk>
2021-03-18 23:10:43 +0000

    serves favicon

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var server = app.listen(3000);
 
 app.use(express.static('public')); 
 
+// serve favicon
+var favicon = require('serve-favicon');
+const path = require('path');
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
+
 // setup socket
 var socket = require('socket.io');
 var io = socket(server);
