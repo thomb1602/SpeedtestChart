576de2b3bd2651c968c22b013f1a22d1ec520839
Tom Bates <thomas1602@hotmail.co.uk>
2021-03-27 22:08:44 +0000

    skeleton downtimereport

---
+++ public/downtimereport.js
@@ -0,0 +1,10 @@
+function getDownTimeReport()
+{
+
+}
+
+function getDownTimeData(startDate, endDate, threshold)
+{
+    const datapoints  = 288; // whole day
+    //var data = await getDataAsync(datapoints, )
+}
