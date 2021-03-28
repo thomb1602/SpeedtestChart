b3c9f4b70ba91b877f18865e8561a1c244bbb4d9
Tom Bates <thomas1602@hotmail.co.uk>
2021-03-28 00:13:38 +0000

    passes form data to getDownTimeData

--- public/downtimereport.js
+++ public/downtimereport.js
@@ -12,6 +12,7 @@ $(function() {
 });
 
 populateForm()
+resetValidation()
 
 async function populateForm()
 {
@@ -23,9 +24,7 @@ async function populateForm()
     var threshold = $select[0].selectize;
 
     start_date.addOption({value: "Today", text: "Today"});
-    // start_date.setValue("Today", "Today");
     end_date.addOption({value: "Today", text: "Today"});
-    // end_date.setValue("Today", "Today");
 
     // get data for date dropdown
     const archiveData = await getArchiveDatesAsync();
@@ -38,13 +37,58 @@ async function populateForm()
     }
 }
 
+function validateForm()
+{
+    var $select = $(document.getElementById('start_date')).selectize();
+    var start_date_select = $select[0].selectize;
+    var $select = $(document.getElementById('end_date')).selectize();
+    var end_date_select = $select[0].selectize;
+
+    const dateRegex = /[0-9]*-[0-9]*-[0-9]*/g
+    const start_date_val = start_date_select.getValue();
+    const end_date_val = end_date_select.getValue();
+    const todays_date = new Date();
+
+    var start_date;
+    var end_date;
+
+    if(start_date_val == "Today") { start_date = todays_date; }
+    else { start_date = new Date(start_date_val.match(dateRegex)); }
+    if(end_date_val == "Today") { end_date = todays_date; }
+    else { end_date = new Date(end_date_val.match(dateRegex)); }
+
+    if(start_date > end_date) {
+        document.getElementById("downtimereport_form").innerText = "Start date must be before end date";
+        return {valid: false, start_date, end_date}; 
+    } 
+    else { 
+        resetValidation()
+        return {valid:true, start_date, end_date}; 
+    }   
+}
+
+function resetValidation()
+{
+    document.getElementById("downtimereport_form").innerText = "";
+}
+
 function getDownTimeReport()
 {
+    const validationData = validateForm();
+    if(validationData.valid)
+    {
+        var $select = $(document.getElementById('threshold')).selectize();
+        var threshold_select = $select[0].selectize;
+        const report_data = getDownTimeData(validationData.start_date, validationData.end_date, threshold_select.getValue());
+    }
 
 }
 
 function getDownTimeData(startDate, endDate, threshold)
 {
     const datapoints  = 288; // whole day
+    var threshold_int;
+    if(threshold == "") { threshold_int = 20; }
+    else { threshold_int = parseInt(threshold); }
     //var data = await getDataAsync(datapoints, )
 }
